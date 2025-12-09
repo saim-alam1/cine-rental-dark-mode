eac67a5dcea0ce43d1a56466701fb21f32b8e653
saim-alam1 <allbehassansaim@gmail.com>
2025-12-09 13:41:55 +0600

    Added A Default Text In CartDetails

--- src/cine/CartDetails.jsx
+++ src/cine/CartDetails.jsx
@@ -24,41 +24,47 @@ export default function CartDetails({ onClose }) {
             Your Carts
           </h2>
           <div className="space-y-8 lg:space-y-12 max-h-[450px] overflow-auto mb-10 lg:mb-14">
-            {cartData.map((item) => (
-              <div className="grid grid-cols-[1fr_auto] gap-4" key={item.id}>
-                <div className="flex items-center gap-4">
-                  <img
-                    className="rounded overflow-hidden"
-                    width={"80px"}
-                    height={"80px"}
-                    src={getImgURL(item.cover)}
-                    alt={item.title}
-                  />
-                  <div>
-                    <h3 className="text-base md:text-xl font-bold">
-                      {item.title}
-                    </h3>
-                    <p className="max-md:text-xs text-[#575A6E]">
-                      {item.genre}
-                    </p>
-                    <span className="max-md:text-xs">${item.price}</span>
+            {cartData.length === 0 ? (
+              <p className="text-3xl text-center font-semibold">
+                The Cart Is Empty
+              </p>
+            ) : (
+              cartData.map((item) => (
+                <div className="grid grid-cols-[1fr_auto] gap-4" key={item.id}>
+                  <div className="flex items-center gap-4">
+                    <img
+                      className="rounded overflow-hidden"
+                      width={"80px"}
+                      height={"80px"}
+                      src={getImgURL(item.cover)}
+                      alt={item.title}
+                    />
+                    <div>
+                      <h3 className="text-base md:text-xl font-bold">
+                        {item.title}
+                      </h3>
+                      <p className="max-md:text-xs text-[#575A6E]">
+                        {item.genre}
+                      </p>
+                      <span className="max-md:text-xs">${item.price}</span>
+                    </div>
+                  </div>
+                  <div className="flex justify-between gap-4 items-center">
+                    <button
+                      className="bg-[#D42967] rounded-md p-2 md:px-4 inline-flex items-center space-x-2 text-white cursor-pointer"
+                      onClick={() => handleDeleteCart(event, item.id)}
+                    >
+                      <img
+                        className="w-5 h-5"
+                        src={deleteIcon}
+                        alt="Delete Icon"
+                      />
+                      <span className="max-md:hidden">Remove</span>
+                    </button>
                   </div>
                 </div>
-                <div className="flex justify-between gap-4 items-center">
-                  <button
-                    className="bg-[#D42967] rounded-md p-2 md:px-4 inline-flex items-center space-x-2 text-white cursor-pointer"
-                    onClick={() => handleDeleteCart(event, item.id)}
-                  >
-                    <img
-                      className="w-5 h-5"
-                      src={deleteIcon}
-                      alt="Delete Icon"
-                    />
-                    <span className="max-md:hidden">Remove</span>
-                  </button>
-                </div>
-              </div>
-            ))}
+              ))
+            )}
           </div>
           <div className="flex items-center justify-end gap-2">
             <button className="rounded-md p-2 md:px-4 inline-flex items-center space-x-2 bg-primary text-[#171923] text-sm cursor-pointer">
